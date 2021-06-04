a8061f6b248df1287474ef52f7cc186f52bdc549
lucy <lucy23059@gmail.com>
2021-06-04 20:06:36 +0800

    6/4

---
+++ threemain.js
@@ -0,0 +1,9 @@
+var scene;
+
+function initThree() {
+	scene = new THREE.Scene();	
+}
+function postMessage (whichAgent, msg) {
+	$('#message').text (whichAgent.name + ': ' + msg);
+	setTimeout (function () {$('#message').text ('')}, 2000);
+}
